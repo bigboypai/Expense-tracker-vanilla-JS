7a77671852ae75f70f9b6920fdb1280335e3af08
bigboypai <81891138+bigboypai@users.noreply.github.com>
2021-06-16 19:30:57 -0600

    Add files via upload

--- code.js
+++ code.js
@@ -1,35 +1,46 @@
-
-const addExpenseBtn = document.querySelector('.add-expense-button')
-const tableTemplate = document.querySelector('.table-template')
-
-
-// Table Logic
-
-function createNewCells() {
-
-    const newRow = tableTemplate.insertRow()
-
-    let nameCell = newRow.insertCell()
-    let dateCell = newRow.insertCell()
-    let amountCell = newRow.insertCell()
-    
-    nameCell.innerHTML = nameInput.value
-    dateCell.innerHTML = dateInput.value
-    amountCell.innerHTML = amountInput.value
+// Class user to add each user
+class User {
+    constructor (name, team, division) {
+        this.name = name;
+        this.team = team;
+        this.division = division;
+    }
 }
 
-// Inputs selector
-const nameInput = document.getElementById('nameInput')
-const dateInput = document.getElementById('dateInput')
-const amountInput = document.getElementById('amountInput')
+const abraham = new User ('Abraham Calvo', 'App Services', 'APTS')
+const eduardo = new User ('Eduardo Echaverri', 'App Services', 'APTS')
+const sebastian = new User ('Sebastian Quiros', 'DevOps', 'OSS')
 
-addExpenseBtn.addEventListener('click', function() {
-    if (nameInput.value !== '' && dateInput.value !== '' && amountInput.value !== '') {
-        createNewCells()
-        nameInput.value = ''
-        dateInput.value = ''
-        amountInput.value = ''
-    } else {
-        swal("We are missing some info!", "It seems like you didn't fill the whole form", 'info');      
-    }
+// Use async to complete the data request
+
+let name = document.getElementById('name')
+let team = document.getElementById('team')
+let division = document.getElementById('division')
+
+function completeData (user) {
+    name.innerText = ' ' + user.name
+    team.innerText = ' ' + user.team
+    division.innerText = ' ' + user.division
+}
+
+// Buttons from the nav-bar
+const profileTab = document.querySelector('.profile-tab')
+const securityTab = document.querySelector('.security-tab')
+
+// Modals to be shown
+
+let profileContainer = document.querySelector('.profile-container')
+let securityContainer = document.querySelector('.security-container')
+
+// Event listeners to add and remove CSS classes
+
+profileTab.addEventListener('click', function() {
+    securityContainer.classList.remove("displayNone")
+    profileContainer.classList.add("displayNone");
+    completeData(abraham)
+})
+
+securityTab.addEventListener('click', function() {
+    profileContainer.classList.remove("displayNone")
+    securityContainer.classList.add("displayNone");
 })
